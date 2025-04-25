3e4cf4a2c88a50a3f6afbf471a1e33870473a36c
Jeyanan <jeyanan07@gmail.com>
2025-04-25 15:19:15 -0400

    Exam 2 fix with the teacher

--- Exam2.java
+++ Exam2.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Write a description of class Exam2 here.
@@ -18,15 +19,19 @@ public class Exam2
     {
         // initialise instance variables
         ArrayList<Double> temperatures = new ArrayList<Double>();
+        Collections.sort(temperatures);                    
+        Double max = Collections.max(temperatures);        
+        Double min = Collections.min(temperatures);       
+        Collections.reverse(temperatures); 
         
         temperatures.add(37.5);
         temperatures.add(32.5);
         temperatures.add(37.9);
         temperatures.add(39.3);
         temperatures.add(47.0);
-        temperatures.add(7.1);
+        temperatures.add(7.1); //
         
-        analyzeTemparature(temperatures);
+        analyzeTemperature(temperatures);
     }
 
     /**
@@ -38,12 +43,17 @@ public class Exam2
    public int sampleMethod(int y)
 {
     // Question 24
+    //(a)
     String str1 = "Jeyanan";
     String str2 = "Jeyanan";
+    
+    //(b)
     if (str1.equals(str2)) {
+        //(c)
         System.out.println("strings are equal");
     } else {
-        
+        //(c)
+        System.out.println("strings are not equal");
     }
     return y;
 }
@@ -51,11 +61,11 @@ public class Exam2
 
     //Question 25 exam 2 
 
-public static void analyzeTemparature(ArrayList<Double> temps) {
+public static void analyzeTemperature(ArrayList<Double> temps) {
     int feverCount = 0;
-    double maxTemp = Double.MIN_VALUE;
+    double maxTemp = 0.0;
 
-    for (double temp : temps) {
+    for (Double temp : temps) {
         if (temp > 37.5) {
             feverCount++;
         }
@@ -66,5 +76,13 @@ public static void analyzeTemparature(ArrayList<Double> temps) {
 
     System.out.println("max fever: ");
     System.out.println("fever count: ");
+    
+    
+    // Question 26
+    System.out.println("Max element (Collections.max): " + Collections.max(temps));
+
+     // Question 27
+    Collections.sort(temps);
+    System.out.println("MAX element (sorted list): " + temps.get(temps.size() - 1));
 }
 }
