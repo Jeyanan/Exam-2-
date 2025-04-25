b1cc40d2ef1668ccb6515c891d2fa42c33c2e983
Jeyanan <jeyanan07@gmail.com>
2025-04-25 13:51:48 -0400

    Initial sharing of project

---
+++ Exam2.java
@@ -0,0 +1,56 @@
+
+/**
+ * Write a description of class Exam2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Exam2
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Exam2
+     */
+    public Exam2()
+    {
+        // initialise instance variables
+        x = 0;
+        
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // Question 24 
+        String str1 = "Jeyanan"
+        String str2 = "Jeyanan"
+        if (str 1,equals (str 2))
+        {
+               system.out println ("strings are equal ")
+        }
+        else  
+    }
+//question 25 
+   public static void analyse Temparature( Arraylist<double> temps{
+     int fever count = 0
+     for (double temp ; temps){
+         if(temps 37.5){
+             for count :
+         }
+        if (temps) max temp)
+        max temp;
+        System.out println("max fever")
+        System.out println("fever count")
+    }
+}
+
+
+}
